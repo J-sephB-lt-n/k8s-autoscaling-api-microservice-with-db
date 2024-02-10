79287ee041b8b2cc025da4e040bd6747306432b7
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-02-10 20:49:11 +0200

    refactor(*): big repo restructure

--- api_gateway/endpoint_routing.py
+++ api_gateway/endpoint_routing.py
@@ -1,6 +1,6 @@
 """Defines routings connecting client requests to internal services"""
 
 endpoint_routing: dict[str, str] = {
-    "is_it_prime/v1": "http://service-endpoints-is-it-prime.default.svc.cluster.local:8080/is_it_prime",
+    "is_it_prime/v1": "http://ksvc-endpoint-is-it-prime.default.svc.cluster.local",
     "postgresql/query/v1": "http://service-endpoints-postgresql-interface.default.svc.cluster.local:8080/query",
 }
